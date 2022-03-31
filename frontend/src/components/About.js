4eeb9c28636d2661f7e55b0ef8b9a96bda4ab7dc
roger <perezmenarogelio@gmail.com>
2022-03-30 21:18:36 -0600

    Frontend

---
+++ frontend/src/components/About.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from "react";
+
+export const About = () => (
+  <Fragment>
+    <h1>About</h1>
+    <p>Somos una empresa de clase mundial la cual se dedica a desarrollar aplicaciones web.</p>
+    <p>Nuestro objetivo es crear soluciones creativas e ingeniosas.</p>
+  </Fragment>
+)
